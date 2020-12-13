f466a1f8adba5cd24f8e379ac5c0fbc691d79d17
unknown <ayugup93@gmail.com>
2020-12-13 15:56:21 +0530

    Initial

---
+++ src/components/pages/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import emmet from "emmet";
+const About = () => {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>About Page</h1>
+                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae, nam veritatis corrupti beatae delectus similique
+                architecto? Quos qui nisi accusantium aut inventore deserunt
+                voluptatum iste debitis sit, exercitationem hic consectetur!</p>
+
+                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae, nam veritatis corrupti beatae delectus similique
+                architecto? Quos qui nisi accusantium aut inventore deserunt
+                voluptatum iste debitis sit, exercitationem hic consectetur!</p>
+
+                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae, nam veritatis corrupti beatae delectus similique
+                architecto? Quos qui nisi accusantium aut inventore deserunt
+                voluptatum iste debitis sit, exercitationem hic consectetur!</p>
+
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default About;
